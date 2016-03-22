03802797b7daec8aca9950393d97af16e0c592fa
brenodamata <bjrdamata@gmail.com>
2016-03-22 14:29:48 -0400

    🌡 Show current weight and change menu

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -2,5 +2,7 @@ class JournalsController < ApplicationController
   def show
     @day_entries = DayEntry.entries_this_month
     @night_entries = NightEntry.entries_this_month
+    @weight = "Weight: #{DayEntry.last.weight} (#{(DayEntry.last.weight-DayEntry.first.weight).round(2)})"
+    @bodyfat = "Bodyfat: #{DayEntry.last.bodyfat}% (#{(DayEntry.last.bodyfat-DayEntry.first.bodyfat).round(2)}%)"
   end
 end
