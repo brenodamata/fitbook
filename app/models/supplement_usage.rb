c6d76fe653df0596634b43b98c050a31c734a81b
brenodamata <bjrdamata@gmail.com>
2016-03-23 17:06:39 -0400

    🍚  Add remaining servings to your current stack

--- app/models/supplement_usage.rb
+++ app/models/supplement_usage.rb
@@ -1,12 +1,13 @@
 class SupplementUsage < ActiveRecord::Base
   belongs_to :user
   belongs_to :supplement
-  after_create :servings
+  before_create :servings
 
 private
   def servings
     if supplement.servings
-      servings = supplement.servings
+      byebug
+      self.remaining_servings = supplement.servings
       # finish_date = servings
     end
   end
